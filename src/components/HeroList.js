f9ae2d243ca65aa81476bcc36edde6900b004877
Christina Loiacono <loiaconochristina@gmail.com>
2021-11-18 21:13:07 -0500

    start HeroList.js

---
+++ src/components/HeroList.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import HeroCard from "./HeroCard";
+
+class HeroList extends Component {
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default 
